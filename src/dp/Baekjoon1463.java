7015f8c3804639d6f789fd3d99fd55930b4cc56a
kyungaaaaaa <kyungaaaaaa1223@gmail.com>
2020-11-02 00:25:43 +0900

    백준 1463 1로만들기

---
+++ src/dp/Baekjoon1463.java
@@ -0,0 +1,26 @@
+package dp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baekjoon1463 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int val = Integer.parseInt(br.readLine());
+        int[] dp=new int[val+1];
+        dp[0]=dp[1]=0;
+
+        for (int i=2;i<=val;i++){
+            dp[i]=dp[i-1]+1;
+            if (i%2==0)dp[i]=Math.min(dp[i/2]+1,dp[i]);
+            if (i%3==0)dp[i]=Math.min(dp[i/3]+1,dp[i]);
+        }
+
+        sb.append(dp[val]);
+        System.out.print(sb);
+
+    }
+}
