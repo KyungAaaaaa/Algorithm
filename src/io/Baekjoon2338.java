ab6c778e6665f90d9ed8bb6eab16e0213d4afe0d
kyungaaaaaa <kyungaaaaaa1223@gmail.com>
2020-11-04 20:32:17 +0900

    백준 2338 긴자리 계산

---
+++ src/io/Baekjoon2338.java
@@ -0,0 +1,17 @@
+package io;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Baekjoon2338 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        BigInteger a = new BigInteger(br.readLine());
+        BigInteger b = new BigInteger(br.readLine());
+        sb.append(a.add(b)).append("\n").append(a.subtract(b)).append("\n").append(a.multiply(b));
+        System.out.print(sb);
+    }
+}
