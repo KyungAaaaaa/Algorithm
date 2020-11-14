606fb33c3970bb23a0749c4c47c642b24c4b823c
kyungaaaaaa <kyungaaaaaa1223@gmail.com>
2020-11-14 15:59:15 +0900

    백준 10872 팩토리얼

---
+++ src/Implementation/Baekjoon10872.java
@@ -0,0 +1,18 @@
+package Implementation;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baekjoon10872 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println(factorial(Integer.parseInt(br.readLine())));
+    }
+
+    public static int factorial(int n) {
+        if (n <= 1) return 1;
+        else return n * factorial(n - 1);
+    }
+}
