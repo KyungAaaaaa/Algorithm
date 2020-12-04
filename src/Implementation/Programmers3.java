2742cd30e199e83b65b0a1d38a12fd748446a274
kyungaaaaaa <kyungaaaaaa1223@gmail.com>
2020-12-05 00:14:46 +0900

    프로그래머스 문자열 내 p와 y의 개수-2

--- src/Implementation/Programmers3.java
+++ src/Implementation/Programmers3.java
@@ -5,15 +5,19 @@ public class Programmers3 {
         String s = "Pyy";
         int sumP = 0;
         int sumY = 0;
-        char[] chars = s.toCharArray();
-        for (char aChar : chars) {
-            if (aChar == 'p' || aChar == 'P') sumP++;
-            else if (aChar == 'y' || aChar == 'Y') sumY++;
-        }
+//        char[] chars = s.toCharArray();
+//        for (char aChar : chars) {
+//            if (aChar == 'p' || aChar == 'P') sumP++;
+//            else if (aChar == 'y' || aChar == 'Y') sumY++;
+//        }
 
 
-        if (sumP == sumY) return true;
-        else return false;
+//        if (sumP == sumY) return true;
+//        else return false;
+//        return sumP == sumY;
+
+        return s.chars().filter(c -> 'p' == c || 'P' == c).count() == s.chars().filter(c -> 'y' == c || 'Y' == c).count();
+        ;
     }
 
 }
