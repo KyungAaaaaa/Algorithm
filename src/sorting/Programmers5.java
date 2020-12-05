53dc3645b72a61df75b3fbb12235db8f84e0def0
kyungaaaaaa <kyungaaaaaa1223@gmail.com>
2020-12-05 12:18:06 +0900

    프로그래머스 K번째 수-2

--- src/sorting/Programmers5.java
+++ src/sorting/Programmers5.java
@@ -7,20 +7,13 @@ class Programmers5 {
         int[] array = {1, 5, 2, 6, 3, 7, 4};
         int[][] commands = {{2, 5, 3}, {4, 4, 1}, {1, 7, 3}};
 
-        int[] result = new int[commands.length];
+        int[] answer = new int[commands.length];
         for (int i = 0; i < commands.length; i++) {
-            int[] arr = subArray(array, commands[i][0], commands[i][1]);
+            int[] arr = Arrays.copyOfRange(array, commands[i][0], commands[i][1]);
             Arrays.sort(arr);
-            result[i] = arr[commands[i][2]-1];
+            answer[i] = arr[commands[i][2] - 1];
         }
+//        return answer;
     }
 
-
-    public static int[] subArray(int[] array, int start, int end) {
-        int[] result = new int[end - start+1];
-        for (int i = 0, j = start-1; j < end; i++, j++) {
-            result[i] = array[j];
-        }
-        return result;
-    }
 }
