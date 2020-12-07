8b6562a9ba4292b2725fabaa2060b53d1fe30cf3
kyungaaaaaa <kyungaaaaaa1223@gmail.com>
2020-12-07 13:06:29 +0900

    프로그래머스 문자열 내 마음대로 정렬하기

---
+++ src/sorting/Programmers9.java
@@ -0,0 +1,20 @@
+package sorting;
+
+import java.util.Arrays;
+
+public class Programmers9 {
+    public static void main(String[] args) {
+        String[] strings = {"abce", "abcd", "cdx"};
+        int n = 2;
+
+        Arrays.sort(strings, (o1, o2) -> {
+            if (o1.charAt(n) == o2.charAt(n))
+                return o1.compareTo(o2);
+            else
+                return o1.charAt(n) - o2.charAt(n);
+        });
+//        return  strings;
+
+
+    }
+}
