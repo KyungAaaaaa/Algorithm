e43269b1f3d7dad726125e8f02ee28e5082183ad
kyungaaaaaa <kyungaaaaaa1223@gmail.com>
2021-08-22 01:27:34 +0900

    프로그래머스 숫자 문자열과 영단어-2

--- src/Implementation/Programmers16.java
+++ src/Implementation/Programmers16.java
@@ -1,79 +1,18 @@
 package Implementation;
 
-import java.util.ArrayList;
-
 public class Programmers16 {
 	public static void main(String[] args) {
 		String s = "onezerotwothreefourfivesixseven";
+		String[] number = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
 
-		ArrayList<Character> ca = new ArrayList<>();
+		for (int i = 0; i < number.length; i++)
+			// 숫자 단어를 가지고 있다면
+			if (s.contains(number[i]))
+				// 해당 단어를 숫자로 replaceAll해준다
+				// i 는 각 숫자단어의 인덱스를 가르키는데, 단어 배열과 인덱스값이 같으므로 i로 변환
+				s = s.replaceAll(number[i], String.valueOf(i));
 
-		char[] charArr = s.toCharArray();
-		for (int i = 0; i < charArr.length; i++) {
-			int num = (int) charArr[i];
-			// 숫자인지 검사
-			if (num >= 48 && num <= 57)
-				// 맞으면 답 배열에 추가
-				ca.add((char) num);
-			else {
-				char alphabet = '0';
-				switch (charArr[i]) {
-				case 'z':
-					i += 3;
-					alphabet = 48;
-					break;
-				case 'o':
-					i += 2;
-					alphabet = 49;
-					break;
-				case 'e':
-					i += 4;
-					alphabet = 56;
-					break;
-				case 'n':
-					i += 3;
-					alphabet = 57;
-					break;
-				case 't':
-					num = charArr[++i];
-					if (num == 'w') {
-						i += 1;
-						alphabet = 50;
-					} else {
-						i += 3;
-						alphabet = 51;
-					}
-					break;
-				case 'f':
-					num = charArr[++i];
-					if (num == 'o')
-
-						alphabet = 52;
-					else
-						alphabet = 53;
-					i += 2;
-					break;
-				case 's':
-					num = charArr[++i];
-					if (num == 'i') {
-						i += 1;
-						alphabet = 54;
-					} else {
-						i += 3;
-						alphabet = 55;
-					}
-					break;
-				default:
-					break;
-				}
-				ca.add(alphabet);
-			}
-		}
-		char[] temp = new char[ca.size()];
-		for (int i = 0; i < temp.length; i++) {
-			temp[i] = ca.get(i);
-		}
-		int answer = Integer.parseInt(String.valueOf(temp));
+		int answer = Integer.parseInt(s);
 		System.out.println(answer);
 	}
 
