668b447139a78aa7ce51c0dfc791cf41339b432e
kyungaaaaaa <kyungaaaaaa1223@gmail.com>
2021-09-03 00:45:56 +0900

    백준 1427 소트인사이드

---
+++ src/sorting/Baekjoon1427.java
@@ -0,0 +1,25 @@
+package sorting;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Baekjoon1427 {
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		String[] input = br.readLine().split("");
+		Arrays.sort(input, Collections.reverseOrder());
+		for (String string : input)
+			bw.append(string);
+
+		bw.flush();
+	}
+
+}
